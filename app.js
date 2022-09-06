fed2743aa1a52466831b34cacf5d49e6fa2632ae
sayedhurhussain <sayedhurhussain55@gmail.com>
2022-09-06 14:58:40 +0500

    Create table in databse mysql Nodejs

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host     : 'localhost',
     user     : 'user',
-    password : 'LoraLora#123'
-    // database : 'nodemysql'
+    password : 'LoraLora#123',
+    database : 'nodemysql'
 });
 
 // Connect
@@ -28,6 +28,18 @@ app.get('/createdb', (req, res) => {
         res.send('Database created...');
     });
 });
+
+// Create tables
+app.get('/createpoststable', (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+
+db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('Posts tables created...');
+    });
+});
+
 
 app.listen('3000', () => {
     console.log("Server started on the port 3000");
